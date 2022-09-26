16a1405e573a7257145fdc61f61d2d23407f18bd
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-09-27 01:32:58 +0600

    Добавлены имена роутам, созданы 3 шаблона для продукта

--- online_store/webapp/urls.py
+++ online_store/webapp/urls.py
@@ -5,8 +5,8 @@ from webapp.views.categories import category_add_view
 
 urlpatterns = [
     path('', products_view),
-    path('products/', products_view),
-    path('products/<id>', product_view),
-    path('categories/add/', category_add_view),
-    path('products/add/', product_add_view)
+    path('products/', products_view, name='products'),
+    path('products/<int:pk>', product_view, name='product_detail'),
+    path('categories/add/', category_add_view, name='add_category'),
+    path('products/add/', product_add_view, name='add_product'),
 ]
--- online_store/webapp/views/products.py
+++ online_store/webapp/views/products.py
@@ -1,12 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from webapp.models import Category, Product
 
 
 
 def products_view(request):
-    return render(request, 'products.html')
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'products.html', context=context)
 
-def product_view(request):
-    return render(request, 'products.html')
+
+def product_view(request, pk):
+    product = Product.objects.get(pk=pk)
+    return render(request, 'product.html', context = {'product': product})
+
 
 def product_add_view(request):
-    return render(request, 'products_add.html')
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        return render(request, 'product_add.html', context = {'categories': categories})
+    category = Category.objects.filter(category=request.POST.get('category'))
+    product = {
+        'product': request.POST.get('product'),
+        'description': request.POST.get('description'),
+        'price': request.POST.get('price'),
+        'product_image': request.POST.get('product_image'),
+        'category': category[0]
+    }
+    new_product = Product.objects.create(**product)
+    return redirect('/')
