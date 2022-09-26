fdc536881318cd921e7606b26d67c464b0e427e2
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-09-27 02:37:29 +0600

    Добавлена вью для добавления категории

--- online_store/webapp/views/categories.py
+++ online_store/webapp/views/categories.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from webapp.models import Category
 
 
 
 def category_add_view(request):
-    return render(request, 'category_add.html')
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        return render(request, 'category_add.html', context = {'categories': categories})
+    category = Category.objects.filter(category=request.POST.get('category'))
+    category = {
+        'category': request.POST.get('category'),
+        'description': request.POST.get('description'),
+    }
+    new_category = Category.objects.create(**category)
+    return redirect('/')
